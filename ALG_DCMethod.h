f7640503ec903bff9bd9a941ffe6b4a3410b2dca
Chenxin <1449850165@qq.com>
2023-03-28 17:28:04 +0800

    DCMethod

--- ALG_DCMethod.h
+++ ALG_DCMethod.h
@@ -1,4 +1,5 @@
 #pragma once
+#define MAX 1025
 #include "Common.h"
 //分治法算法设计
 
@@ -126,4 +127,135 @@ void postpart(int& s, int& t)
 {
 	t = t;
 	s = (s + t) / 2;
+}
+
+long max3(long a, long b, long c)
+{
+	long max = a > b ? a : b;
+	return max > c ? max : c;
+}
+
+//求a[left..high]序列中最大连续子序列和
+long maxSubSum(int a[], int left, int right)
+{
+	int i, j;
+	long maxLeftSum, maxRightSum;
+	long maxLeftBorderSum, leftBorderSum;
+	long maxRightBorderSum, rightBorderSum;
+
+	if (left == right)     // 如果子序列只有一个元素
+	{
+		if (a[left] > 0)
+			return a[left];
+		else
+			return 0;
+	}
+	int mid = (left + right) / 2;
+	maxLeftSum = maxSubSum(a, left, mid);
+	maxRightSum = maxSubSum(a, mid + 1, right);
+	maxLeftBorderSum = 0; leftBorderSum = 0;
+	for (i = mid; i >= left; i--)
+	{
+		leftBorderSum += a[i];
+		if (leftBorderSum > maxLeftBorderSum)
+			maxLeftBorderSum = leftBorderSum;
+	}
+	maxRightBorderSum = 0; rightBorderSum = 0;
+	for (j = mid+1; j<=right; j++)
+	{
+		rightBorderSum += a[i];
+		if (rightBorderSum > maxRightBorderSum)
+			maxRightBorderSum = rightBorderSum;
+	}
+
+	return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
+}
+
+//求解棋盘覆盖的问题
+//问题表示
+int k;        //棋盘大小       
+int x, y;   //特殊方格的位置
+	
+//求解问题表示
+int board[MAX][MAX];
+int tile = 1;
+
+//棋盘覆盖问题
+void ChessBoard(int tr, int tc, int dr, int dc, int size)
+{
+	if (size == 1)return;           //递归出口
+	int t = tile++;       //取一个L型骨，其牌号为tile
+	int s = size / 2;      //分割棋盘
+
+	//考虑左上角象限
+	if (dr < tr + s && dc < tc + s)       //特殊方格在此象限中
+		ChessBoard(tr, tc, dr, dc, s);
+	else
+	{
+		board[tr + s - 1][tc + s - 1] = t; //用t号L型骨牌覆盖右下角
+		ChessBoard(tr, tc, tr + s - 1, tc + s - 1, s);
+		//将右下角作为特殊方格继续处理象限
+	}
+
+	//考虑右上角象限
+	if (dr < tr + s && dc >= tc + s)
+		ChessBoard(tr, tc + s, dr, dc, s);		//特殊方格在此象限中 
+	else					//此象限中无特殊方格
+	{
+		board[tr + s - 1][tc + s] = t;		//用t号L型骨牌覆盖左下角
+		ChessBoard(tr, tc + s, tr + s - 1, tc + s, s);
+		//将左下角作为特殊方格继续处理该象限
+	}
+
+	//处理左下角象限
+	if (dr >= tr + s && dc < tc + s)		//特殊方格在此象限中
+		ChessBoard(tr + s, tc, dr, dc, s);
+	else				//此象限中无特殊方格
+	{
+		board[tr + s][tc + s - 1] = t;  	//用t号L型骨牌覆盖右上角
+		ChessBoard(tr + s, tc, tr + s, tc + s - 1, s);
+		//将右上角作为特殊方格继续处理该象限
+	}
+
+	//处理右下角象限
+	if (dr >= tr + s && dc >= tc + s)		//特殊方格在此象限中
+		ChessBoard(tr + s, tc + s, dr, dc, s);
+	else				//此象限中无特殊方格
+	{
+		board[tr + s][tc + s] = t;  	//用t号L型骨牌覆盖左上角
+		ChessBoard(tr + s, tc + s, tr + s, tc + s, s);
+		//将左上角作为特殊方格继续处理该象限
+	}
+}
+
+//求解循环日程安排问题
+/* 设有n=2k个选手要进行网球循环赛，要求设计一个满足以下要求的比赛日程表：
+     （1）每个选手必须与其他n-1个选手各赛一次。
+     （2）每个选手一天只能赛一次。
+     （3）循环赛在n-1天之内结束。
+ */
+#define MAX 101
+int k;         //求解结果表示
+int a[MAX][MAX];     //存放比赛日程表
+void Plan(int k)
+{
+	int i, j, n, t, temp;
+	n = 2;			//n从2^1=2开始
+	a[1][1] = 1; a[1][2] = 2;   	//求解2个选手比赛日程,得到左上角元素
+	a[2][1] = 2; a[2][2] = 1;
+	for (t = 1; t < k; t++)		//迭代处理2^2(t=1)…,2^k(t=k-1)个选手
+	{
+		temp = n;					//temp=2^t 上一个方块长度
+		n = n * 2; 					//n=2^(t+1)   下一个方块
+		for (i = temp + 1; i <= n; i++)		//填左下角元素
+			for (j = 1; j <= temp; j++)
+				a[i][j] = a[i - temp][j] + temp; 	//产生左下角元素
+		for (i = 1; i <= temp; i++)		//填右上角元素
+			for (j = temp + 1; j <= n; j++)
+				a[i][j] = a[i + temp][(j + temp) % n];
+		for (i = temp + 1; i <= n; i++)		//填右下角元素
+			for (j = temp + 1; j <= n; j++)
+				a[i][j] = a[i - temp][j - temp];
+	}
+
 }
