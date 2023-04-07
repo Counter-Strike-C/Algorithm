8e27a20aebde6ed5320d1f5a72e03749eed94449
Chenxin <1449850165@qq.com>
2023-04-07 16:53:38 +0800

    Back Tracking ALG

--- ALG_BTMethod.cpp
+++ ALG_BTMethod.cpp
@@ -164,4 +164,56 @@ void mDfs(int i)        //求解图的m着色问题
 	}
 }
 
-//求解任务分配的问题
+//求解任务分配的问题,(0,0)不用
+int x3[MAXN];     //临时解
+int n4 = 4;
+int c2[MAXN][MAXN] = { {0},{0,9,2,7,8},{0,6,4,3,7},
+				   {0,5,8,1,8},{0,7,6,9,4} };   //任务成本矩阵
+int cost = 0;     //临时解的成本
+int bestx[MAXN];       //最优解
+int mincost = INF;      //最优解成本
+bool worker[MAXN];          //worker[j]表示任务j是否已经分配人员
+
+void dfs3(int i)        //为第i个人分配任务
+{
+	const int m = i;
+	if (i > n4)      //到达叶子节点后
+	{
+		if (cost < mincost)          //比较求最优解
+		{
+			mincost =cost;
+			for (int j = 1; j <= n4; j++)
+				bestx[j] = x3[j];
+		}
+	}
+	else
+	{
+		for (int j = 1; j < n4; j++)
+		{
+			worker[j] = true;
+			x3[j] = 1;
+			cost += c2[m][j];  //人物i分配任务j的花费
+			dfs3(i + 1);
+			worker[j] = false;    //回溯
+			x3[j] = 0;
+			cost -= c2[m][j];
+
+		}
+	}
+}
+
+
+//5、求解活动安排问题
+struct Action
+{
+	int b;				//活动起始时间
+	int e;				//活动结束时间
+};
+int n5 = 4;
+Action A[] = { {0,0},{1,3},{2,5},{4,8},{6,10} }; //下标0不用
+
+int x5[MAX];     //临时解向量
+int bestx[MAX];       //最优解向量
+int laste = 0;        //一个调度方案中最后兼容活动的结束时间,初值为0
+int sum = 0;		//一个调度方案中所有兼容活动个数,初值为0
+int maxsum = 0;
