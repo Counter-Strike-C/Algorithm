9a47d430f0923cea9136cea794a03d3a3433d79e
Chenxin <1449850165@qq.com>
2023-04-20 17:31:47 +0800

    a

--- ALG_BPMethod.cpp
+++ ALG_BPMethod.cpp
@@ -1,1 +1,42 @@
 #include "ALG_BPMethod.h"
+
+int Fib1(int n)
+{
+	dp[1] = dp[2] = 1;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+}
+
+//整数拆分问题
+void Split(int n, int k)
+{
+	for(int i=1;i <= n;i++)
+		for (int j = 1; j <= k; k++)
+		{
+			if (i == 1 && j == 1)
+				dp1[i][j] = 1;
+			else if (i < j)
+				dp1[i][j] = dp1[i][i];
+			else if (i == j)
+				dp1[i][j] = dp1[i][j - 1];
+			else if (i > j)
+				dp1[i][j] = dp1[i - j][j] + dp1[i][j - 1];
+		}
+}
+
+//递归
+int  Split1(int n, int k)
+{
+	if (n == 1 && k == 1)
+		return 1;
+	else if (n < k)
+		return Split1(n, n);
+	else if (n == k)
+		return Split1(n, n - 1);
+	else if (n > k)
+		return Split1(n - k, k) + Split1(n, k - 1);
+}
--- ALG_BPMethod.h
+++ ALG_BPMethod.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "Common.h"
 //动态规划
-class ALG_BPMethod
-{
-public:
-	int dp[MAX];
-	int count = 1;       //累计调用的步骤
-	int Fib1(int n);       //斐波那契数列求解
-};
 
+int dp[MAX];
+int count = 1;       //累计调用的步骤
+int Fib1(int n);
+
+//求将正整数n无序拆分成最大数为k（称为n的k拆分）的拆分方案个数，
+//要求所有的拆分方案不重复。
+int dp1[MAXN][MAXN];         //动态规划数组
+void Split(int n, int k);       //求解算法
