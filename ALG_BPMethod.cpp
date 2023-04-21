6b7aca476c6a8bee63fd5c9f82f5dddf1848d978
Chenxin <1449850165@qq.com>
2023-04-21 17:42:45 +0800

    BP

--- ALG_BPMethod.cpp
+++ ALG_BPMethod.cpp
@@ -40,3 +40,91 @@ int  Split1(int n, int k)
 	else if (n > k)
 		return Split1(n - k, k) + Split1(n, k - 1);
 }
+
+//自顶向下的动态规划算法，备忘录方法
+int Split2(int n, int k)
+{
+	if (dp1[n][k] != 0)return dp1[n][k];    //出口
+	if (n == 1 && k == 1)
+	{
+		return dp1[n][k] = 1;
+
+	}
+	else if (n < k)
+		return dp1[n][k] = Split1(n, n);
+	else if (n == k)
+		return dp1[n][k] = Split1(n, n - 1);
+	else if (n > k)
+		return dp1[n][k] = Split1(n - k, k) + Split1(n, k - 1);
+}
+//这种方法是一种递归算法，其执行过程也是自顶向下的，但当某个子问题解求出后，
+//将其结果存放在一张表（dp）中，而且相同的子问题只计算一次，
+//在后面需要时只有简单查表，以避免大量的重复计算。
+//这种方法称之为备忘录方法（memorization method）。
+
+
+//2、求解最大连续子序列和问题
+void maxSubSum()
+{
+	dp2[0] = 0;
+	for (int j = 1; j <= n; j++)
+		dp2[j] = max(dp2[j - 1] + a[j], a[j]);
+}
+
+//3、求解最短路径和的问题
+int Search()       //求最短路径和ans
+{
+	int i, j;
+	dp3[0][0]=a3[0][0];
+	for (int i = 1; i <= n; i++)    //考虑j=0的情况
+	{
+		dp3[i][0] = dp3[i - 1][0] + a3[i][0];
+		pre[i][0] = i - 1;
+	}
+		
+
+	for (int i = 1; i <= n; i++)       //考虑i=j的情况
+	{
+		dp3[i][i] = dp3[i - 1][i - 1] + a3[i][i];
+		pre[i][i] = i - 1;
+	}
+		
+
+	for(int i=2;i<n;i++)
+		for (int j = 1; j < n; j++)
+		{
+			if (a3[i - 1][j - 1] > a3[i - 1][j])
+			{
+				dp3[i][j] = dp3[i - 1][j] + a3[i][j];
+				pre[i][j] = j;
+			}
+			else
+			{
+				dp3[i][j] = dp3[i - 1][j-1] + a3[i][j];
+				pre[i][j] = j-1;
+			}
+		}
+	int ans = dp3[n - 1][0];
+	int k = 0;
+	for (int i = i; i <= n; i++)
+		if (dp3[n - 1][0] < ans)
+			k = i;
+	return k;      //k就是最短路径终点，pre存储最短路径
+
+}
+
+void disppath(int k)  //输出最小和路径
+{
+	int i = n - 1;
+	vector<int> path;
+	while (i>=0)
+	{
+		path.push_back(a3[i][k]);
+		k = pre[i][k];     //读取前驱节点
+		i--;
+	}
+	vector<int>::reverse_iterator it;
+	for (it = path.rbegin(); it != path.rend(); ++it)
+		cout << *it;    //反向输出构成正向路径
+	cout << endl;
+}
--- ALG_BPMethod.h
+++ ALG_BPMethod.h
@@ -5,8 +5,23 @@
 int dp[MAX];
 int count = 1;       //累计调用的步骤
 int Fib1(int n);
-
+int n = 6;
 //求将正整数n无序拆分成最大数为k（称为n的k拆分）的拆分方案个数，
 //要求所有的拆分方案不重复。
 int dp1[MAXN][MAXN];         //动态规划数组
 void Split(int n, int k);       //求解算法
+
+//求解最大连续子序列和问题, 问题表示
+
+int a[] = { 0, -2,11,-4,13, -5, -2 };	//a数组不用下标为0的元素
+int  dp2[MAXN];
+void maxSubSum();
+
+//求解三角形最小路径和问题
+//问题表示
+int a3[MAXN][MAXN];
+//求解结果表示
+int ans = 0;
+int dp3[MAXN][MAXN];
+int pre[MAXN][MAXN];
+int Search();   //搜索
