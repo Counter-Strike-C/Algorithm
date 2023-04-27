325e78bbd657124ff238f251712b640aa9e57410
Chenxin <1449850165@qq.com>
2023-04-27 17:03:17 +0800

    BAB

--- ALG_BaBMethod.cpp
+++ ALG_BaBMethod.cpp
@@ -8,3 +8,135 @@ void ALG_BaBMethod::bfs_2()
 	e.length = 0;
 	pqu.push(e);   //源节点进队
 }
+struct NodeType_1		//队列结点类型
+{
+	int no;			//结点编号
+	int i;			//人员编号
+	int x[MAXN];		//x[i]为人员i分配的任务编号
+	bool worker[MAXN];		//worker[i]=true表示任务i已经分配
+	int cost;			//已经分配任务所需要的成本
+	int lb;			//下界
+	bool operator<(const NodeType_1& s) const	//重载<关系函数
+	{
+		return lb > s.lb;
+	}
+};
+
+int c[MAXN][MAXN] = { {0},{0,9,2,7,8},{0,6,4,3,7},
+	{0,5,8,1,8},{0,7,6,9,4} };
+//下标0的元素不用
+int bestx[MAXN];		//最优分配方案
+int mincost = INF;		//最小成本
+int total = 1;			//结点个数累计
+
+//分枝限界法求解任务分配问题
+void debug_1()
+{
+	n = 4;
+
+}
+
+//求下界的函数
+void bound(NodeType_1& e)
+{
+	
+	int minsum = 0;
+	for (int i1 = e.i + 1; i1 <= n; i1++)   //考虑未分配的人员
+	{
+		int minc = INF;
+		for (int j1 = 1; j1 <= n; j1++)      //遍历某人的成本
+		{
+			if (e.worker[j1] == false && c[i1][j1] <= minc)  //如果任务没有分配,找到最小的成本
+			{
+				minc = c[i1][j1];
+			}
+			minsum += minc;
+		}
+	}
+	e.lb = e.cost + minsum;
+}
+
+void bfs_3()          //求解任务分配
+{
+	int j;
+	NodeType_1 e, e1;
+	priority_queue<NodeType_1> qu;
+	memset(e.x, 0, sizeof(e.x));
+	memset(e.worker, 0, sizeof(e.worker));  //初始化根节点的worker
+
+	e.i = 0;    //根节点指定人员为0
+	e.no = total++;
+	e.cost = 0;    
+	bound(e);
+	qu.push(e);       //根节点入队
+
+	while (!qu.empty())  //
+	{
+		e = qu.top();
+		qu.pop();      //出队节点e考虑当前人员i
+		if (e.i == n)    //若达到叶子节点
+		{
+			if (e.cost < mincost)   //比较求最优解
+			{
+				mincost = e.cost;
+				for (int i = 0; i <= n; i++) //找到最优解，复制解向量
+					bestx[i] = e.x[i];
+			}
+		}
+		e1.i = e.i + 1;  //分配下一个人员
+		for (int j = 1; j <= n; j++)
+		{
+			if (e.worker[j] == 1)
+				continue;
+			for (int i = 1; i <= n; i++)
+				e1.x[i] = e.x[i];   //复制解向量
+			e1.x[e1.i] = j;      //分配任务j
+			for (int i2 = 1; i2 <= n; i2++)
+				e1.worker[i2] = e.worker[i2];
+			e1.worker[j] = true;     //任务j已经分配
+			e1.cost = e.cost + c[e1.i][j];
+			bound(e1);      //求e1的边界
+			e1.no = total++;
+			if (e1.lb < mincost)      //剪枝
+				qu.push(e1);
+		}
+
+	}
+}
+
+int a[MAX] = { 0,5,12,4,8 };	//M1上的执行时间,不用下标0的元素
+int b[MAX] = { 0,6,2,14,7 };	//M2上的执行时间,不用下标0的元素
+
+//求解结果表示
+int bestf = INF;			//存放最优调度时间
+//int bestx[MAX];		//存放当前作业最佳调度
+//int total = 1;			//结点个数累计
+
+//求解流水线作业调度问题
+void debug_2()
+{
+	n = 4;
+	free(bestx);
+	total = 1;
+}
+
+struct NodeType_2		//队列结点类型
+{
+	int no;			//结点编号
+	int x[MAX];			//x[i]表示第i步分配作业编号
+	int y[MAX];			//y[i]=1表示编号为i的作业已经分配
+	int i;			//步骤编号
+	int f1;			//已经分配作业M1的执行时间
+	int f2;			//已经分配作业M2的执行时间
+	int lb;			//下界
+	bool operator<(const NodeType_2& s) const	//重载<关系函数
+	{
+		return lb > s.lb;	//lb越小越优先出队
+	}
+};
+
+//边界函数
+void bound_2(NodeType_2 &e)
+{
+
+}
--- ALG_MapMethod.cpp
+++ ALG_MapMethod.cpp
@@ -6,6 +6,7 @@
 
 typedef set<int> SET;   //定义集合
 MGraph g;      //图的邻接矩阵
+int minpathlen = INF;
 //使用动态规划方法解决旅行家问题
 void debug1()
 {
@@ -28,7 +29,7 @@ void debug1()
 
 int f(SET V, int i)
 {
-	int minpathlen = INF;
+
 	if (V.size() == 0)   //当v为空时,直接返回当前边
 		return g.edges[0][i];
 	else     //集合不为空时
@@ -47,14 +48,138 @@ int f(SET V, int i)
 }
 
 //回溯法求解tsp问题
+int s = 0;
+vector<int>minpath;     //最短路径数组
 void debug2()
 {
-	//问题表示
-	int s;    
-
+	minpath.clear();
+	minpathlen = INF;
 	//求解问题表示
 	int Count = 1;   //路径条数累计
-	vector<int>minpath;     //最短路径数组
-	int minpathlen = INF;
+	int A[][MAXV] = {						//一个带权有向图
+		{0,8,5,36}, {6,0,8,5}, {8,9,0,5}, {7,7,8,0} };
+	int n = 4, e = 12;
+	CreateMat(g, A, n, e);		//创建图的邻接矩阵存储结构g
+	s = 0;					//起始顶点为0
+	vector<int> path;
+	int pathlen = 0;
+	SET V;
+	for (int i = 1; i < g.n; i++)			//插入1,2,3顶
+		V.insert(i);
+	TSP2(V, 0, path, pathlen);
+}
 
+//回溯法
+void TSP2(SET V,int i,vector<int> path,int pathlen)
+{
+	int prev; 
+	if (path.size() > 0)
+		prev = path.back();        //prev为路径最后一个顶点
+	path.push_back(i);     //添加当前顶点i
+	pathlen += g.edges[prev][i];      //累计路径长度
+	if (V.size() == 0)      //找到一个叶子节点
+	{
+		if (g.edges[i][s] != 0 && g.edges[i][s] != INF)  //如果顶点i到起点s之间有边
+		{
+			path.push_back(0);   //路径加入起点0
+			pathlen += g.edges[i][s];     //累计路径长度
+			if (pathlen < minpathlen)
+			{
+				minpathlen = pathlen;
+				minpath = path;
+			}
+		}
+	}
+	else
+	{
+		SET::iterator it;
+		for (it = V.begin(); it != V.end(); it++)
+		{
+			SET tmpV = V;
+			int j = *it;
+			tmpV.erase(j);
+			if (pathlen < minpathlen)
+				minpathlen = pathlen;
+		}
+	}
+}
+
+//分枝限界法求解问题
+struct NodeType3			//队列结点类型
+{
+	int v;				//当前顶点
+	int num;				//路径中的结点个数
+	vector<int> path;			//当前路径
+	int pathlen;			//当前路径长度
+	int visited[MAXV];			//顶点访问标记
+	bool operator<(const NodeType3& s) const
+	{
+		return pathlen > s.pathlen;	//pathlen越小越优先出队
+	}
+};
+struct NodeType3;    //队列节点类型
+void debug3()
+{
+	s = 0;
+
+	int Count = 1;
+	vector<int> minpath;     //保存最短路径
+	minpathlen = INF;
+	
+}
+
+//分枝限界法
+void TSP3()
+{
+	NodeType3 e, e1;
+	priority_queue<NodeType3> qu;        
+	e.v = 0;          //根节点e
+	e.pathlen = 0;
+	e.path.push_back(0);
+	e.num = 0;
+	memset(e.visited, 0, sizeof(e.visited));
+	e.visited[0] = 1;
+	qu.push(e);       //根节点e进队
+
+	while (!qu.empty())      //队不空循环
+	{
+		e = qu.top();
+		qu.pop();      //出队节点e
+		if (e.num == g.n)   //到达叶子节点
+		{
+			if (g.edges[e.v][s] != 0 && g.edges[e.v][s] != INF)   //如果e到根节点s之间有边
+			{
+				e.path.push_back(s);      //将起点加入到路径中
+				e.pathlen += g.edges[e.v][s];       //另外计入从e.v到起点s的路径长度
+				if (e.pathlen < minpathlen)    //更新最短路径
+				{
+					minpathlen = e.pathlen;
+					minpath = e.path;
+				}
+			}
+		}
+		else  //非叶子节点
+		{
+			for (int j = 1; j < g.n; j++)    //顶点1到顶点j循环
+			{
+				if (g.edges[e.v][j] != 0 && g.edges[e.v][j] != INF)     //当前顶点到顶点j有边
+				{
+					if (e.visited[j] == 1)//跳过重复访问顶点
+						continue;
+					e1.v = j;
+					e1.num = e.num + 1;
+					e1.path = e.path;
+					e1.path.push_back(j);
+					e1.pathlen = e.pathlen + g.edges[e.v][j];
+					for (int i = 0; i < g.n; i++)
+						e1.visited[i] = e.visited[i];
+					if (e1.pathlen < minpathlen)      //剪枝
+					{
+						e1.visited[j] = 1;
+						qu.push(e1);
+					}
+				}
+			}
+		}
+	}
 }
